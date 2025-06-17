677c58e3cfd69ef6e4ce5b76866d7f61f63fbf01
Tobs <68722387+Anotherparttimer@users.noreply.github.com>
2025-06-17 10:32:12 -0400

    errror fix

--- scanner.py
+++ scanner.py
@@ -8,8 +8,8 @@ result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
 print(result.stdout)
 
 for ping in range (1,10):
-    address = "127.0.0" + str(ping)
-    res = subprocess.call(['ping','-c','3', address])
+    address = "127.0.0." + str(ping)
+    res = subprocess.call(['ping','-n','3', address])
     if res == 0 :
         print("ping to ", address,"OK")
     elif res == 2:
