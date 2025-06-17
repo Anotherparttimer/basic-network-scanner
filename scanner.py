43e1a6e74c46a9e8d43d61221a8c0d158d6da005
Tobs <68722387+Anotherparttimer@users.noreply.github.com>
2025-06-17 10:02:05 -0400

    inital up

---
+++ scanner.py
@@ -0,0 +1,19 @@
+import subprocess
+import socket
+
+
+print("hello")
+
+result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
+print(result.stdout)
+
+for ping in range (1,10):
+    address = "127.0.0" + str(ping)
+    res = subprocess.call(['ping','-c','3', address])
+    if res == 0 :
+        print("ping to ", address,"OK")
+    elif res == 2:
+        print("no response from", address)
+    else:
+        print("ping to ",address,"failed ")
+
